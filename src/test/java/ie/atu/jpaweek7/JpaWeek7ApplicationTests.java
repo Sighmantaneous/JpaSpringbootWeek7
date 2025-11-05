1888d675b4cb96e00ea5bf5f51d83c19def49ecc
sighman <g00286398@atu.ie>
2025-11-05 12:54:52 +0000

    Setting up project with Jpa, h2 database

---
+++ src/test/java/ie/atu/jpaweek7/JpaWeek7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.jpaweek7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWeek7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
